3995ed46c396dca016dfadce45cec33760027cb4
serhiy.krasovskyy <xhunter74@gmail.com>
2025-03-24 13:50:13 +0200

    Implemented customer controller.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MultiTenantDbContext.Data.Admin;
+using MultiTenantDbContext.Data.Customer;
+using MultiTenantDbContext.Services;
+
+namespace MultiTenantDbContext.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly CustomerDbContext _customerDbContext;
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly AdminDbContext _adminDbContext;
+
+        public CustomersController(CustomerDbContext customerDbContext,
+            AdminDbContext adminDbContext,
+            IServiceScopeFactory scopeFactory)
+        {
+            _customerDbContext = customerDbContext;
+            _scopeFactory = scopeFactory;
+            _adminDbContext = adminDbContext;
+        }
+
+        [HttpGet("data")]
+        public async Task<IActionResult> Get()
+        {
+            var data = await _customerDbContext.Data.ToListAsync();
+            return Ok(data);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> GetAllCustomersData()
+        {
+            var data = new List<Data.Customer.Data>();
+            var customers = await _adminDbContext.Customers.ToListAsync();
+            foreach (var customer in customers)
+            {
+                using var scope = _scopeFactory.CreateScope();
+                var customerIdService = scope.ServiceProvider.GetRequiredService<ICustomerIdService>();
+                customerIdService.CustomerId = customer.Id;
+                var customerDbContext = scope.ServiceProvider.GetRequiredService<CustomerDbContext>();
+                var customerData = await customerDbContext.Data.FirstOrDefaultAsync();
+                data.Add(customerData);
+            }
+            return Ok(data);
+        }
+    }
+}
