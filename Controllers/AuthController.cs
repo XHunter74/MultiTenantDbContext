8594857faa7af0e3e3108f9a5ff7f5cdc9766386
serhiy.krasovskyy <xhunter74@gmail.com>
2025-03-24 11:38:56 +0200

    Implemented login.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using MultiTenantDbContext.Data.Admin;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -10,24 +12,37 @@ namespace MultiTenantDbContext.Controllers;
 [Route("api/[controller]")]
 public class AuthController : ControllerBase
 {
-    [HttpPost("login")]
-    public IActionResult Login()
+    private readonly AdminDbContext _adminDbContext;
+
+    public AuthController(AdminDbContext adminDbContext)
     {
-        var token = GenerateJwtToken();
+        _adminDbContext = adminDbContext;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromQuery] string user)
+    {
+        var userInDb = await _adminDbContext.Users.FirstOrDefaultAsync(u => u.Name.ToLower() == user);
+        if (userInDb == null)
+        {
+            return Unauthorized();
+        }
+        var token = GenerateJwtToken(userInDb.Name, userInDb.CustomerId);
         return Ok(new { token });
     }
 
-    private string GenerateJwtToken()
+    private string GenerateJwtToken(string userName, Guid customerId)
     {
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Constants.SecretKey));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-        var customerIdClaim = new Claim("CustomerId", "123");
+        var customerIdClaim = new Claim("CustomerId", customerId.ToString());
+        var userNameClaim = new Claim(ClaimTypes.Name, userName);
 
         var token = new JwtSecurityToken(
             issuer: "your_issuer",
             audience: "your_audience",
-            claims: new List<Claim> { customerIdClaim },
+            claims: new List<Claim> { userNameClaim, customerIdClaim },
             expires: DateTime.Now.AddMinutes(30),
             signingCredentials: creds);
 
