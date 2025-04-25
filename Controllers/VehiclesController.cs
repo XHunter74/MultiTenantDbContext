e171753f6c1d66b2e259dcc7bfe49aadc9b9fdcd
serhiy.krasovskyy <xhunter74@gmail.com>
2025-04-25 20:55:33 +0300

    Added API method to get vehicles depending on vehicle type.

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MultiTenantDbContext.Data.Customer;
+using MultiTenantDbContext.Factories;
+
+namespace MultiTenantDbContext.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class VehiclesController : ControllerBase
+{
+    private readonly CustomerDbContext _customerDbContext;
+
+    public VehiclesController(CustomerDbContext customerDbContext)
+    {
+        _customerDbContext = customerDbContext;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetVehicles([FromQuery] VehicleType vehicleType)
+    {
+        try
+        {
+            var query = VehicleQueryFactory.GetVehicleQuery(_customerDbContext, vehicleType);
+            var result = await query.ToListAsync();
+            return Ok(result);
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+}
---
+++ Factories/VehicleQueryFactory.cs
@@ -0,0 +1,17 @@
+﻿using MultiTenantDbContext.Data.Customer;
+
+namespace MultiTenantDbContext.Factories;
+
+public static class VehicleQueryFactory
+{
+    public static IQueryable<object> GetVehicleQuery(CustomerDbContext context, VehicleType vehicleType)
+    {
+        return vehicleType switch
+        {
+            VehicleType.Car => context.Vehicles.OfType<Car>(),
+            VehicleType.Truck => context.Vehicles.OfType<Truck>(),
+            VehicleType.Motorcycle => context.Vehicles.OfType<Motorcycle>(),
+            _ => throw new ArgumentException("Unsupported vehicle type", nameof(vehicleType))
+        };
+    }
+}
